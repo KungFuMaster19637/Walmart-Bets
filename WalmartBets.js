178774968e0ee5ef2768d69258a5faf85666bf09
Deon <deon.lee19637@gmail.com>
2023-01-13 18:43:03 +0100

    Fixed Backgrounds

--- WalmartBets.js
+++ WalmartBets.js
@@ -1,19 +1,38 @@
-function readTextFile(file)
-{
+function readTextFile(file) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
-                return(allText);
+                return (allText);
             }
         }
     }
     rawFile.send(null);
 }
 
-readTextFile("BalanceNotes/Cycle2.txt"); 
+function collapseNav() {
+    let nav = document.getElementById("myNavBar");
+    if (nav.className === "navBar") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "navBar";
+    }
+}
+/*
+let navbar = document.getElementById("myNavBar");
+let sticky = navbar.offsetTop;
+window.onscroll = function() {stickyNav()};
+
+function stickyNav() {
+    console.log(window.scrollY);
+  if (window.scrollY >= sticky) {
+    alert("smoll");
+    navbar.classList.add("sticky");
+  } else {
+    alert("smoll");
+
+    navbar.classList.remove("sticky");
+  }
+}*/
