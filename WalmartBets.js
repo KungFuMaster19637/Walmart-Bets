87b7f6c4a026ca0191b5e8ad2e406e8d6eb958ae
Deon <deon.lee19637@gmail.com>
2023-01-25 18:10:48 +0100

    Images Fixed

--- WalmartBets.js
+++ WalmartBets.js
@@ -1,10 +1,10 @@
 //#region Loading Shop and Balance Notes ---------------------------------------------
 function readTextFile(file) {
-    let rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4) {
-            if (rawFile.status === 200 || rawFile.status == 0) {
+    let connection = new XMLHttpRequest();
+    connection.open("GET", file, false);
+    connection.onreadystatechange = function () {
+        if (connection.readyState === 4) {
+            if (connection.status === 200 || connection.status == 0) {
 
                 // Removes all text of the balancetext div
                 const resetText = document.getElementById("balance-text");
@@ -13,7 +13,7 @@ function readTextFile(file) {
                 }
 
                 // Writes the new text of balancetext div
-                let allText = rawFile.responseText;
+                let allText = connection.responseText;
                 let lines = allText.split('\n');
                 for (let line = 0; line < lines.length; line++) {
                     let paragraph;
@@ -30,7 +30,7 @@ function readTextFile(file) {
             }
         }
     }
-    rawFile.send(null);
+    connection.send(null);
 }
 
 function loadShop(cycle) {
@@ -79,9 +79,9 @@ function collapseNav() {
 
 //#region Loading Leaderboard
 
-function readFromExcel() {
+function readFromExcel(excelFile) {
     let connection = new XMLHttpRequest();
-    connection.open("GET", "/ProjectWB/leaderboard.xlsx", true);
+    connection.open("GET", excelFile, true);
     connection.responseType = "blob";
     connection.onload = function () {
         let fullData = [];
@@ -118,8 +118,8 @@ function ProcessExcel(data) {
         leaderboardObject.playerName = excelRows[i].Player;
         leaderboardObject.video = excelRows[i].Video;
         leaderboardObject.time = excelRows[i].Time;
-        leaderboardObject.team1 = ` ${excelRows[i].Character1} - ${excelRows[i].Character2} - ${excelRows[i].Character3} - ${excelRows[i].Character4}`;
-        leaderboardObject.team2 = ` ${excelRows[i].Character5} - ${excelRows[i].Character6} - ${excelRows[i].Character7} - ${excelRows[i].Character8}`;
+        leaderboardObject.team1 = `${excelRows[i].Character1}-${excelRows[i].Character2}-${excelRows[i].Character3}-${excelRows[i].Character4}`;
+        leaderboardObject.team2 = `${excelRows[i].Character5}-${excelRows[i].Character6}-${excelRows[i].Character7}-${excelRows[i].Character8}`;
         fullData.push(leaderboardObject);
     }
 
@@ -127,38 +127,55 @@ function ProcessExcel(data) {
         return "<i class='fa fa-youtube-play'></i>";
     };
 
+    let imageFormatter = function (cell, formatterParams, onRendered, team) {
+
+        let imageString = "";
+        for (let i = 0; i < 4; i++) {
+            imageString += `<img class = 'leaderboard-character-image' 
+            src='${loadPictures(cell.getValue().split('-').map(item => item.trim())[i])}'>`;
+        }
+        return imageString;
+    };
+
     let table = new Tabulator("#leaderboard-table", {
         layout: "fitColumns",
+        height: "100%",
+        responsiveLayout: "hide",
         autoResize: true,
         initialSort: [
             { column: "time", dir: "asc" }, //sort by this first
         ],
         columnDefaults: {
-            resizable: false
+            resizable: false,
+            vertAlign: "middle",
+            hozAlign: "center",
+            headerHozAlign: "center",
+            headerSort: false
         },
         columns: [
-            { title: "Total Time", field: "time", width: "10%", hozAlign: "center", headerHozAlign: "center" },
-            { title: "Name", field: "playerName", width: "12%"},
-            { title: "Video", field: "video", width: "8%", hozAlign: "center", headerHozAlign: "center", headerSort: false, 
+            { title: "Total Time", field: "time", width: "10%",  headerSort: true },
+            { title: "Name", field: "playerName", width: "12%", headerSort: true },
+            { title: "Video", field: "video", width: "8%",
                 formatter: buttonFormatter, cellClick: function (e, cell) {
                     let Btn = document.createElement('Button');
-                    Btn.id = "Btn_Id";
-                    Btn.onclick = window.open(cell.getRow().getData().video);
+                    Btn.id = "video-link-button";
+                    Btn.onclick = window.open(cell.getValue());
                 }
             },
-            { title: "Team 1", field: "team1", headerSort: false},
-            { title: "Team 2", field: "team2", headerSort: false},
+            { title: "Team 1", field: "team1", formatter: imageFormatter },
+            { title: "Team 2", field: "team2", formatter: imageFormatter },
         ],
         data: fullData,
     });
-
-
-    //Need Function to load in pictures:
-
     table.on("tableBuilt", () => {
         table.setPage(2);
     });
 
+}
+
+function loadPictures(imageName) {
+    let fullLink = "/ProjectWB/Images/Characters/" + imageName + ".png"
+    return fullLink;
 }
 
 //#endregion
